378ce298aa42f809d68d5d0b50110a12b7b4c761
Advay Chandorkar <advay.chandorkar@gmail.com>
2024-08-23 01:39:29 -0400

    updates

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <title>Advay Chandorkar</title>
+          <title>Advay Chandorkar<img src="/favicon.png"></img></title>
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,7 +39,7 @@ export default function Home() {
       variants={fadeIn}
     >
       <div className="h-screen w-full bg-neutral-950 bg-grid-white/[0.021] relative flex items-center justify-center">
-        <motion.div variants={fadeIn}>
+        <motion.div variants={fadeIn} className="relative">
           <motion.h1 
             className="text-5xl font-bold text-center text-white mb-6"
             variants={fadeIn}
@@ -50,26 +50,34 @@ export default function Home() {
             className="flex flex-col items-center justify-center space-y-2"
             variants={fadeIn}
           >
-          <div className="flex flex-col leading-relaxed text-primary">
-						<p>
-							i'm a 15 year old developer from <Link href={"https://www.google.com/maps/place/Mississauga,+ON,+Canada/@43.5774568,-79.6591567,11z/data=!3m1!4b1!4m6!3m5!1s0x882b469fe76b05b7:0x3146cbed75966db!8m2!3d43.5852972!4d-79.6449838!16zL20vMDE1NGd4?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D"}>
-              Mississauga, ON</Link> with a passion for engineering and problem solving.
-						</p>
-            <p className="mt-1">
-              currently i'm a grade 11 IB student at <Link href="https://glenforest.peelschools.org/about-us">Glenforest SS</Link>.
-              At the moment, i'm working at <Link href="futuremd.tech">FutureMD</Link>.
-            </p>
-            <p className="mt-1 justify-center items-center text-center">
-              to learn more about me, click here!
-            </p>
-					</div>
+            <div className="flex flex-col leading-relaxed text-primary text-center">
+              <p>
+                i'm a 15 year old developer from <Link href={"https://www.google.com/maps/place/Mississauga,+ON,+Canada/@43.5774568,-79.6591567,11z/data=!3m1!4b1!4m6!3m5!1s0x882b469fe76b05b7:0x3146cbed75966db!8m2!3d43.5852972!4d-79.6449838!16zL20vMDE1NGd4?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D"}>
+                  Mississauga, ON</Link> with a passion for engineering and problem solving.
+              </p>
+              <p className="mt-1">
+                currently i'm a grade 11 IB student at <Link href="https://glenforest.peelschools.org/about-us">Glenforest SS</Link>.
+                At the moment, i'm working at <Link href="futuremd.tech">FutureMD</Link>.
+              </p>
+              <p className="mt-1">
+                to learn more about me, click here!
+              </p>
+            </div>
+          </motion.div>
+          <div className="absolute left-1/2 transform -translate-x-1/2 mt-4 flex gap-4">
             <File
               setWindowOpen={() => setIsTerminalOpen(true)}
               className="px-2"
               filename="learn.exe"
               imageSrc="/computer.png"
             />
-          </motion.div>
+            <File
+              setWindowOpen={() => setIsTerminalOpen(true)}
+              className="px-2"
+              filename="projects.app"
+              imageSrc="/files.png"
+            />
+          </div>
           {isTerminalOpen && (
             <motion.div
               initial={{ opacity: 0 }}
