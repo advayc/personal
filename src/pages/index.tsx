ba6cb644ad930b1bac98e1274cae4130b6e6a4a8
Advay Chandorkar <advay.chandorkar@gmail.com>
2026-05-16 20:57:28 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -337,12 +337,12 @@ export default function Home() {
           >
             <div className="flex flex-col leading-relaxed text-primary text-center text-[14px] sm:text-base px-1" style={{fontFamily}}>
               <p>
-                i&apos;m a {AGE} year old full-stack developer from <Link href={"https://www.google.com/maps/place/Mississauga,+ON,+Canada/@43.5774568,-79.6591567,11z/data=!3m1!4b1!4m6!3m5!1s0x882b469fe76b05b7:0x3146cbed75966db!8m2!3d43.5852972!4d-79.6449838!16zL20vMDE1NGd4?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D"}>
-                  Mississauga, ON</Link> and i like building things and solving problems
+                i&apos;m a {AGE} year old full-stack developer from <Link href={"https://www.google.com/maps/search/Toronto,%20Ontario,%20Canada"}>
+                  Toronto, ON</Link> and i like building things and solving problems
               </p>
               <p className="mt-1">
-                right now, i&apos;m a grade 12 ib student and
-                i&apos;m working on <Link href="https://sitemaker.advay.ca/">sitemaker</Link> & <Link href="https://spy.advay.ca/">spy</Link>
+                right now, i&apos;m a grade 12 high school student and
+                i&apos;m working on <Link href="https://github.com/Seva-Eats">seva eats</Link>
               </p>
               <p className="mt-1">
                 to learn more about me, click the files - or view my resume <Link href="/resume.pdf">here</Link>.
